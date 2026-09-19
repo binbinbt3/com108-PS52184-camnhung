afe31d45c56742e28d57a42f59f6bcc5fd3c366f
binbinbt3 <binbinbt3@gmail.com>
2026-09-19 10:29:21 +0700

    Lab 1

---
+++ lab1/Bai1_helloc.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("nguyen thi cam nhung\n");
+    printf("PS52184\n");
+    return 0;
+}
---
+++ lab1/bai2_kieudulieu.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char mssv[]="PS52184";
+    char hoten[]="nguyen thi cam nhung";
+    int namsinh=2008;
+    float diemtrungbinh=8.5;
+    int tuoi=2026-namsinh;
+    printf("ma so sinh vien: %s\n",mssv);
+    printf("ho va ten: %s\n",hoten);
+    printf("nam sinh: %d\n",namsinh);
+    printf("tuoi: %d\n",tuoi);
+    printf("diem trung binh: %.2f\n",diemtrungbinh);
+    return 0;
+        
+}
