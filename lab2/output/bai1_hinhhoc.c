668dd89ca6e2f51d6f52d7c1924fb91a723e9da3
binbinbt3 <binbinbt3@gmail.com>
2026-09-26 10:58:49 +0700

    hoan thanh lap 2

---
+++ lab2/output/bai1_hinhhoc.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define PI 3.14159
+
+int main() {
+    float chieuDai, chieuRong;
+    float banKinh;
+    float chuViCN, dienTichCN;
+    float chuViTron, dienTichTron;
+
+    // Hình chữ nhật
+    printf("Nhap chieu dai: ");
+    scanf("%f", &chieuDai);
+
+    printf("Nhap chieu rong: ");
+    scanf("%f", &chieuRong);
+
+    chuViCN = (chieuDai + chieuRong) * 2;
+    dienTichCN = chieuDai * chieuRong;
+
+    printf("Chu vi hinh chu nhat: %.2f\n", chuViCN);
+    printf("Dien tich hinh chu nhat: %.2f\n", dienTichCN);
+
+    // Hình tròn
+    printf("Nhap ban kinh: ");
+    scanf("%f", &banKinh);
+
+    chuViTron = 2 * PI * banKinh;
+    dienTichTron = PI * banKinh * banKinh;
+
+    printf("Chu vi hinh tron: %.2f\n", chuViTron);
+    printf("Dien tich hinh tron: %.2f\n", dienTichTron);
+
+    return 0;
+}
